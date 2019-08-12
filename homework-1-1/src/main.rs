12f2b0718923ab0a2739058ee84d5753cc4f25e5
Alexander Lyon <arlyon@me.com>
2019-08-12 20:00:27 +0100

    Initial commit

---
+++ homework-1-1/src/main.rs
@@ -0,0 +1,95 @@
+/*
+Problem 1: Adding up numbers
+----------------------------
+
+You have an input file whose content is as follows (ignoring the obvious
+start/end markers):
+
+    ---8<---
+    14
+    9
+    7
+    1
+    -5
+    0
+    -2
+    9
+    1
+    -100
+    75623
+    -65536
+    -100
+    5
+    2
+    100
+    46
+    ---8<---
+
+Your goal is to write a program which when run as follows:
+
+    $ cargo run -- inputfile
+
+will read `inputfile`, break it into lines, parse each line as a number of some
+useful type, sum all the numbers, and print the sum to stdout.
+
+If any of the lines does not parse as a suitable number, please just return
+the error out of your main function, using the techniques we learned in the
+10 green bottles example.
+
+Bonus points if you make interesting use of `Iterator` methods.
+
+Note: When I test your homework, I won't use the above input, so no cheating.
+*/
+
+/*
+QUESTIONS:
+
+I'd be keen to learn if there is a nicer way to express
+"map and then sum into a Result, collecting all errors from the map"
+Result has a sum method, but it only return the first error...
+*/
+
+use std::env;
+use std::fs;
+use std::fmt;
+use std::error;
+
+#[derive(Debug)]
+struct ArgError {
+    pub msg: String,
+}
+
+impl error::Error for ArgError {}
+
+impl fmt::Display for ArgError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.msg)
+    }
+}
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    let file_name = env::args().nth(1)
+        .ok_or(ArgError { msg: "No file name provided!".to_string() })?;
+
+    let file_content = fs::read_to_string(&file_name)?;
+
+    // Ok(line number, Result) and Err(line_number, Result)
+    let (oks, errors): (Vec<_>, Vec<_>) = file_content.split('\n')
+        .map(str::parse::<i32>)
+        .enumerate()
+        .partition(|(_, f)| f.is_ok());
+
+    let numbers = oks.iter().map(|(_, f)| f.as_ref().unwrap());
+    let mut errors = errors.iter().map(|(i, f)| (i+1, f.as_ref().unwrap_err())).peekable();
+
+    if errors.peek().is_none() {
+        Ok(println!("{}", numbers.sum::<i32>()))
+    } else {
+        Err(Box::new(ArgError{
+            msg: format!("Errors found during parse: {}", errors
+                .map(|(i, e)| format!("on line {} ({:})", i, e.to_string()))
+                .collect::<Vec<String>>()
+                .join(", "))
+        }))
+    }
+}
---
+++ homework-1-2/src/main.rs
@@ -0,0 +1,88 @@
+/*
+Problem 2 - In for the long haul
+--------------------------------
+
+You now have an input file with the following content:
+
+    ---8<---
+    This is your input file
+
+    Anything which is not one of the important characters will be ignored
+    by the program
+
+    The program is as follows:
+
+       +[-
+         [<<
+          [+
+           [--->]
+          -[<<<]
+         ]
+        ]>>>-
+       ]
+
+    I'm guessing that you know where we're going with all this
+
+       >-.---.>..>.<<<<-.<+.>>>>>.>.<<.<-.
+
+    But perhaps you're as lost as a little lamb?
+    ---8<---
+
+As you can no doubt tell, this is a program in the [Brainfuck][] language.
+
+For now, we're not going to be writing anything as complex as a Brainfuck
+interpreter, though we will be building one in the future so bear that
+in mind as you write today's homework answer.
+
+[Brainfuck]: https://en.wikipedia.org/wiki/Brainfuck
+
+For today, please write a program which, when run as:
+
+    $ cargo run -- inputfile
+
+will read `inputfile`, reduce it to the list of valid Brainfuck characters
+(i.e. strip out all of the comment characters) and then print out the
+program.  There are eight valid command characters, and they're listed in
+the Wikipedia article.
+
+You can do this entirely with things we've already considered, by means of
+iterators you can find on `BufRead` and on `str`, and with judicious use of
+`match` and `print!()/println!()`
+
+However, for bonus points, make use of `Vec<char>`, `Vec<u8>` or `String` and
+learn how to use `collect()` from the `Iterator` trait.
+
+Note: When I test your homework, I'll use different input, so no cheating.
+
+*/
+
+use std::env;
+use std::fs;
+use std::error;
+use std::fmt;
+
+#[derive(Debug)]
+struct ArgError {
+    pub msg: String,
+}
+
+impl error::Error for ArgError {}
+
+impl fmt::Display for ArgError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.msg)
+    }
+}
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    let file_name = env::args().nth(1)
+        .ok_or(ArgError { msg: "No file name provided!".to_string() })?;
+
+    let content = fs::read_to_string(&file_name)?;
+
+    let filtered_content = content.chars()
+        .filter(|c1| "<>+-.,[]".chars().any(|c2| c2.eq(c1)))
+        .collect::<String>();
+
+    Ok(println!("{}", filtered_content))
+}
